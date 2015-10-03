645a49289c37de01dc573d024db84bcad519083f
veitsi <xtfkpi@gmail.com>
2015-10-03 19:13:30 +0300

    parsing & matching

--- js/extractgv.js
+++ js/extractgv.js
@@ -1,24 +1,25 @@
-var txt = "\n Java programming experience and SQL experience a must; experience with any of the below is a bonus.\n Full stack experience (all aspects of software development and software life cycle)\n Bachelors degree in computer science or electrical engineering\n Expertise in Java and enterprise Java\n Experienced with Spring framework\n Experienced with ORM\n Good understanding of relational database theory\n Working knowledge of SQL\n Swing\x2FVaadin experience major plus\n Mybatis major plus\n";
+var txt = "\n java programming experience and SQL experience a must; experience with any of the below is a bonus.\n Full stack experience (all aspects of software development and software life cycle)\n Bachelors degree in computer science or electrical engineering\n Expertise in Java and enterprise Java\n Experienced with Spring framework\n Experienced with ORM\n Good understanding of relational database theory\n Working knowledge of SQL\n Swing\x2FVaadin experience major plus\n mybatis major plus\n";
 
 var sentence = 'По просьбам некоторых читателей решил написать топик про контекст в javascript',
     mas = [], j = 0;
-
-document.write(extractgv(txt));
+extractgv(txt);
 
 function extractgv(txt) {
+    console.log('in extractgv');
     var skillsSet = ['java', 'sql', 'ember', 'bootstrap', 'mybatis', 'javascript', 'orm'];
     var skills = [];
     var possibleSkills = mysplit(txt);
-
-    return matchgv(skillsSet,possibleSkills);
+    console.log('possible skills'+possibleSkills);
+    console.log('found in vacancy '+matchArr(skillsSet,possibleSkills));
+    return matchArr(skillsSet,possibleSkills);
 }
 
-function matchgv(v,r){
+function matchArr(v,r){
     var skills=[], counter=0, i, lv=v.length, j, lr= r.length;
     for (i=0;i<lv;i++){
         for (j=0;j<lr;j++){
-            if (v[i]===r[j]){
-                skills.push(v[i]);
+            if (v[i].toLowerCase()===r[j].toLowerCase()){
+                skills[counter]=v[i];
                 counter++;
                 break;
                 //we can delete r[j] after this
@@ -37,7 +38,7 @@ for (i = 0; i < str.length; i++) {
         continue;
     }
     else {
-        mas[j] ? mas[j] += str[i] : mas[j] = str[i];
+        mas[j] ? mas[j] += str[i].toLowerCase() : mas[j] = str[i].toLowerCase();
     }
 }
 return mas;
